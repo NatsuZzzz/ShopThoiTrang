145e06fb9924ebc489677f6d5cbe48fdb08d0b8b
Natsu <phamdatytb2006@gmail.com>
2025-07-18 19:41:12 +0700

    🎉 Upload dự án Java đầu tay lên GitHub

---
+++ src/main/java/Shop/dao/DuAnDauTay.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Shop.dao;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DuAnDauTay {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
